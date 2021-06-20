dbcaffe8376beec535b05e5a138c8fa8b0837436
george_terezakis <georget@eight8.gr>
2021-06-20 17:04:03 +0300

    change name for website

--- core/views.py
+++ core/views.py
@@ -265,7 +265,7 @@ class CustomDatasetMloperation(APIView):
 
     def get(self,request,filename,sensitive,analysis,target,privileged,unprivileged):
         
-        url="https://server3-test.herokuapp.com/files/" + filename + '.csv' 
+        url="https://bias-auditing-tool.herokuapp.com/files/" + filename + '.csv' 
         df_credit = pd.read_csv(url)
         df_credit_not_encoded = df_credit
         df_credit = df_credit.drop(df_credit.columns[0], axis=1)
